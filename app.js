f4b8c29dc2bb2e88800ef6449a3b68d7aaa5c630
Anirban Banerjee <abanerjee763@outlook.com>
2021-08-17 19:42:59 +0530

    first commit

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.use(cookieParser());
 // Serve static assests, if in production
 if (process.env.NODE_ENV === "production") {
     //set static folder
-    app.use(express.static("client/build"));
+    app.use(express.static("socialmediafrontend/build"));
   
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "socialmediafrontend", "build", "index.html"));
     });
   }
 
